baf0384a4294fbf707e44de91139091992ca5c39
wony5248 <qjawls0501@naver.com>
2025-11-11 01:50:31 +0900

    second

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // 프론트엔드에서 호출하려면 CORS 허용
+  app.enableCors({
+    origin: 'https://dambit-client.vercel.app',
+    credentials: true,
+  }); // 프론트엔드에서 호출하려면 CORS 허용
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
